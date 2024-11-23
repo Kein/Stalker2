e19399e06561495420b554cd28f3ed07a03a836d
Kein <kein-of@yandex.ru>
2024-11-23 20:57:07 +0300

    Fied uproject and re-added Wwise

--- Source/Stalker2.Target.cs
+++ Source/Stalker2.Target.cs
@@ -1,11 +1,12 @@
 ﻿using UnrealBuildTool;
 
-public class Stalker2GameTarget : TargetRules {
-	public Stalker2GameTarget(TargetInfo Target) : base(Target) {
+public class Stalker2Target : TargetRules {
+	public Stalker2Target(TargetInfo Target) : base(Target) {
 
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
 		ExtraModuleNames.AddRange(new string[]
 		{
 			"MediaIOCore",
